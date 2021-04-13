0005a4bb60356bc146a355d5f0e20dc9c3b4fe8b
Matthijs Douze <matthijs@fb.com>
2021-04-13 08:54:29 +0000

    some ARM timings

--- speed_test.py
+++ speed_test.py
@@ -1,6 +1,6 @@
 # srun --gres=gpu:8 --partition dev --constraint=volta16gb -c80 -t100 --mem 500GB python -u speed_test.py | tee timings
 import os
-import apex
+# import apex
 import torch
 import torchvision
 import time
@@ -54,7 +54,7 @@ def compute_throughput_cuda(name, model, device, batch_size, resolution=224):
           'images/s @ batch size', batch_size)
 
 
-for device in ['cuda:0', 'cpu']:
+for device in "cpu", : # ['cuda:0', 'cpu']:
     if device == 'cpu':
         os.system('echo -n "nb processors "; '
                   'cat /proc/cpuinfo | grep ^processor | wc -l; '
